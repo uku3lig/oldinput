1e68ca1db9a4462e495fe0556314a0c1ab83967c
uku <uku3lig@gmail.com>
2022-08-13 12:34:00 +0200

    1.1.1+1.12.2

--- src/main/java/net/uku3lig/oldinput/OldInput.java
+++ src/main/java/net/uku3lig/oldinput/OldInput.java
@@ -19,7 +19,7 @@ import java.util.stream.Collectors;
 public class OldInput extends MouseHelper {
     public static final String MOD_ID = "oldinput";
     public static final String MOD_NAME = "OldInput";
-    public static final String VERSION = "1.1.0";
+    public static final String VERSION = "1.1.1";
 
     private static final ScheduledExecutorService executor = Executors.newScheduledThreadPool(8);
 
