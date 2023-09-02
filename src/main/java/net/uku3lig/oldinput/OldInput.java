6dc7b08fb8324ca891e127a140b389feba83b5cf
uku <uku3lig@gmail.com>
2023-09-02 18:25:42 +0200

    bump version to 1.1.3

--- src/main/java/net/uku3lig/oldinput/OldInput.java
+++ src/main/java/net/uku3lig/oldinput/OldInput.java
@@ -22,7 +22,7 @@ import java.util.stream.Collectors;
 public class OldInput extends MouseHelper {
     public static final String MOD_ID = "oldinput";
     public static final String MOD_NAME = "OldInput";
-    public static final String VERSION = "1.1.2";
+    public static final String VERSION = "1.1.3";
 
     private static final ScheduledExecutorService executor = Executors.newScheduledThreadPool(8);
 
